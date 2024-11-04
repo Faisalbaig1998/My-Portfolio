f8df19ffd44c70773dd354ae223ae2b981a30973
Faisal <mirzaf317@gmail.com>
2024-11-04 22:55:37 +0530

    First commit

---
+++ src/components/Profile.js
@@ -0,0 +1,13 @@
+import "./Profile.css";
+import { useEffect } from "react";
+
+function Profile() {
+  return (
+    <>
+      <h2 id="name-tag">Faisal Baig</h2>
+      <p id="short-desc">I am a Frontend Developer</p>
+    </>
+  );
+}
+
+export default Profile;
