f0b6508f5afbd751c99ebbaca7df2e16dae1e569
Faisal <mirzaf317@gmail.com>
2024-11-05 22:22:01 +0530

    second commit

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -3,10 +3,10 @@ import { useEffect } from "react";
 
 function Profile() {
   return (
-    <>
+    <div id="container">
       <h2 id="name-tag">Faisal Baig</h2>
       <p id="short-desc">I am a Frontend Developer</p>
-    </>
+    </div>
   );
 }
 
